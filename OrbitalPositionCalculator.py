bc9a30a792f4dfc788ba3c7829492b188061047e
Carter-TE <cte12001@gmail.com>
2020-07-28 14:04:36 -0700

    Updated classes to v. 1.8
    Alpha version 1.8
    See README for version's functionality

--- OrbitalPositionCalculator.py
+++ OrbitalPositionCalculator.py
@@ -1,5 +1,5 @@
 import math as math
-import OrbitalGraph
+from scipy import integrate
 
 
 ''' For all calculations work from center of body 
@@ -8,10 +8,11 @@ import OrbitalGraph
     
         for x in times:
         print(x)
-    '''
+'''
+
 
 class Orbit_Calculator():
-    def __init__(self, body, pos, a, p, ):
+    def __init__(self, body, pos, a, p, bapo=True):
         self.apo = a + body.radius  # Apoapsis to center
         self.peri = p + body.radius  # Periapsis to center
 
@@ -20,90 +21,41 @@ class Orbit_Calculator():
         self.a = (self.apo + self.peri) / 2  # Semi-major axis
         self.e = (self.apo - self.peri) / (self.apo + self.peri)
         self.T = (int)(2 * math.pi * (self.a ** 3 / (6.673e-11 * self.mass)) ** .5)
+        self.del_t = 0
 
         self.true_anomaly = 0
+        self.area = 0
+        self.bapo = bapo
+        self.init_t_anon = 0
 
-
-    # Calculates the estimated elapsed time since Periapsis
-    # Used to help calculate orbital position given initial position
-    '''def elapsed_time(self,bapo):
-        # creates array of times where values are (index - 1) seconds
-        # First half of orbit
-        if bapo:
-            count = 0
-            times = range(1, (int)(self.T/2)+1)
-            for x in times:
-                print(x)
-                count+=1
-            print(count)
-        # Second half of orbit
-        else:
-            times = [None]* ((int)(self.T+1)-(int)(self.T / 2) + 1)
-            count = self.T
-            print(times)
-
-            for x in range(((int)(self.T+1)-(int)(self.T / 2) + 1)):
-                times[(int)(x)] = count
-                count -= 1
-
-        # Binary search through possible times to find elapsed time
-        print(times)
-        delta_t = self.binary_search(times, 0, len(times)-1, self.init_pos, bapo)
-        return delta_t
-
-    # Helper method used to help calculate initial elapsed time
-    # Use binary search on times array, comparing the position at a given delta t to the initial position inputted
-    def binary_search(self, arr, lo, hi, ans, bapo):
-        mid = (hi+lo)//2
-
-        error = self.calc_error(bapo)
-        nu = self.calc_true_anom(self.e, self.pos, bapo)
-
-        upper_bound = self.calculate_Gposition(nu+error)  # position calculated using maximum error in true anomaly
-        lower_bound = self.calculate_Gposition(nu-error)  # position calculated using minimum error in true anomaly
-        temp_pos = self.pos_from_peri(arr[mid], bapo)     # estimated position based off of time from binary search
-
-        # Binary search
-        #  +- 5 meters to upper and lower bound (wiggle room)
-        if temp_pos <= upper_bound+5 and temp_pos >=lower_bound-5:
-            return arr[mid]
-        elif temp_pos > upper_bound+5:
-            return self.binary_search(arr, lo, mid-1, ans, bapo)
-        else:
-            return self.binary_search(arr, mid + 1, hi, ans, bapo)'''
-
+    # Calculates ellapsed time in orbit using position values
+    # Should only be used when delta_t is null
     def elapsed_time(self, bapo):
         nu = self.calc_true_anom(self.pos, bapo)
         E = self.calc_ecc_anom(nu, bapo)
         M0 = self.calc_imean_anom(E)
         n = self.calc_mean_motion()
-        time = int(((2 * math.pi) - M0)/n)
-        return self.T - time
-
-    # Helper method to estimate final position after a given change in time from periapsis
-    def pos_from_peri(self, t, bapo):
-        nu = self.approx_tru_anom(self.e, t)
-        r = (self.a * (1 - (self.e ** 2))) / (1 + (self.e * math.cos(nu)))
-        return (int)(r)
+        time = int(((2 * math.pi) - M0) / n)
+        self.del_t = self.T - time
+        return self.del_t
 
     # Calculates the error for the true anomaly
     # The error is of the order e3
-    def calc_error(self, bapo):
-        nu = self.calc_true_anom(self.pos, bapo)
-        error = nu * (self.e**3)
+    def calc_error(self, nu=None, bapo=None):
+        if nu is None:
+            nu = self.calc_true_anom(self.pos, bapo)
+        error = nu * (self.e ** 3)
         return error
 
     def calc_semimajor_axis(self, ra, rp):
         return (ra + rp) / 2
 
-
     def calc_eccentricity(self, ra, rp):
         return (ra - rp) / (ra + rp)
 
-
     #  Calculates the True anomaly using position
     #  Currently used to calculate initial true anomaly from initial position
-    def calc_true_anom(self, p0 = None, bapo = True):
+    def calc_true_anom(self, p0=None, bapo=True):
         if p0 is None:
             p0 = self.pos
         num1 = (self.a * (1 - self.e ** 2)) / p0
@@ -120,7 +72,6 @@ class Orbit_Calculator():
         self.true_anomaly = nu
         return nu
 
-
     #  Calculates the eccentric anomaly using the true anomaly
     #  Currently used to calculate the initial eccentric anomaly using the initial true anomaly
     def calc_ecc_anom(self, nu, bapo):
@@ -130,22 +81,19 @@ class Orbit_Calculator():
             return (2 * math.pi) - eccentric_anom
         return eccentric_anom
 
-
     def calc_mean_motion(self):
         num1 = self.mass * 6.67E-11
         num1 = num1 / (self.a ** 3)
         n = num1 ** (1 / 2)
         return n
 
-
     #  Calculates mean anomaly using the eccentric anomaly
     #  Currently used to calculate the initial mean anomaly based of the initial eccentric anomaly
     def calc_imean_anom(self, Eanom):
         return Eanom - (self.e * math.sin(Eanom))
 
-
     #  Calculates Mean anomaly after delta t
-    def calc_mean_anom(self, t, bapo, nu0 = None):
+    def calc_mean_anom(self, t, bapo, nu0=None):
         if nu0 is None:
             nu0 = self.calc_true_anom(bapo=bapo)
         E0 = self.calc_ecc_anom(nu0, bapo)
@@ -157,63 +105,80 @@ class Orbit_Calculator():
         # print("Final Mean Anomaly:\t\t",M)
         return M
 
-
     #  Approximates true anomaly after delta t
     #  Used to calculate estimated position after delta t
-    def approx_tru_anom(self, t, bapo, nu0 = None):
+    def approx_tru_anom(self, t, bapo, nu0=None):
         m = self.calc_mean_anom(t, bapo, nu0)
-        #print("predicted mean anom", m)
+        # print("predicted mean anom", m)
         nu = m + (2 * self.e * math.sin(m)) + (1.25 * self.e ** 2 * math.sin(2 * m))
         return nu
 
+    # Callable helper method to adjust accuracy of true anomalies
+    def make_accurate(self, nu, prev):
+        hi = nu + .001
+        lo = nu - .001
+        area = self.area  # * (self.del_t-1)
+        accurate_nu = self.accuracy_BS(nu, hi, lo, area, prev)
+        return accurate_nu
+
+    # Uses a binary search to make approximated true anomaly more accurate
+    # Not explicitly called
+    def accuracy_BS(self, nu, hi, lo, area, prev):
+        temp_area = self.calculate_area(prev, nu)
+        if temp_area > area + 500:
+            hi = nu
+            nu = (hi + lo) / 2
+            return self.accuracy_BS(nu, hi, lo, area, prev)
+
+        elif temp_area < area - 500:
+            lo = nu
+            nu = (hi + lo) / 2
+            return self.accuracy_BS(nu, hi, lo, area, prev)
+
+        else:
+            return nu
 
     #  Calculates future position based on change in time (essentially r(t), actually r(nu))
     def calculate_position(self, t, bapo):
         nu = self.approx_tru_anom(t, bapo)
-        r = (self.a * (1 - (self.e ** 2))) / (1 + (self.e * math.cos(nu)))
+        true_anon = self.make_accurate(nu, bapo)
+        r = (self.a * (1 - (self.e ** 2))) / (1 + (self.e * math.cos(true_anon)))
         return round(r)
 
-    #Calculates position given true anomaly
+    # Calculates position given true anomaly
     # Used to draw ellipse of orbit in OrbitalGraph.py
-    def calculate_Gposition(self,  nu):
+    def calculate_Gposition(self, nu):
         r = (self.a * (1 - (self.e ** 2))) / (1 + (self.e * math.cos(nu)))
         return round(r)
 
+    # Calculates area swept out in any part of the orbit
+    # start and end are angles, specifically the initial and final true anomalies of a sector
+    def calculate_area(self, start, end):
+        eqs = lambda nu: .5 * ((self.a * (1 - (self.e ** 2))) / (1 + (self.e * math.cos(nu)))) ** 2
+        ans = integrate.quad(eqs, start, end)
+        return ans[0]
 
-if __name__ == '__main__':
-    '''
-    mass = float(input("Enter mass of primary body: "))
-    radius = float(input("Enter radius of primary body: "))
-    apo = float(input("Enter distance from Apoapsis to surface (m): "))
-    peri = float(input("Enter distance from Periapsis to surface (m): "))
-    pos = float(input("Enter satellites initial position: "))
-    del_t = float(input("Enter change in time: "))
-    bapo = input("Before apoapsis: (T/F)")
+    # Creates an array of size T (period in seconds) and finds the true anomaly at every second in an orbit
+    def init_angles(self):
+        t_anomalies = [None] * (self.T + 1)
+        t_anomalies[0] = 0
+        t_anomalies[1] = self.init_t_anon
+        t_anomalies[self.T // 2] = math.pi
+        t_anomalies[self.T] = 2 * math.pi
+        bapo = True
+        for i in range(self.T):
+            if i == 0 or i == 1 or i == self.T // 2 or i == self.T:
+                continue
+            if i > self.T // 2:
+                bapo = False
+            prev = t_anomalies[i - 1]
+            current = self.approx_tru_anom(1, bapo, prev)
+            try:
+                current = self.make_accurate(current, prev)
+            except Exception as e:
+                print(i, e)
+            t_anomalies[i] = current
 
-    #  TEST 1 VALUES
-    '''
-    mass = 2.646e19
-    radius = 60000
-    apo = 117674
-    peri = 6701
-    pos = 117674
-    del_t = 3193
-    bapo = True
-    #  Answer should be close to peri (current ans = 1709801)
-
-
-    # radA = apo + radius     # Apoapsis to center
-    # radP = peri + radius    # Periapsis to center
-    p0 = pos + radius       # Initial position to center
+        return t_anomalies
 
 
-
-    #OrbitalGraph.graph(a,e, radius)
-
-    '''position = calculate_position(a, e, mass, del_t, p0, bapo)
-    # print("Predicted position: ", round(position) - radius)
-    itruanom = calc_true_anom(a, e, 91065+radius, True)
-    print(itruanom)
-    E = calc_ecc_anom(e, itruanom, True)
-    print("Eccentric anomaly: ", E)
-    print("Mean anomaly", calc_imean_anom(e, E))'''
--- Satellite.py
+++ Satellite.py
@@ -1,27 +1,17 @@
 import OrbitalPositionCalculator as calc
-import OrbitalGraph as grapher
-from matplotlib import pyplot as plt
 import matplotlib
 import math
-matplotlib.use("TkAgg")
-
 
 class Body(object):
-
     def __init__(self, rad, mass):
         self.radius = rad
         self.mass = mass
 
 
 class Satellite(object):
-
     def __init__(self, planet, apo, peri, pos = None, bapo=True):
 
-
-
         self.before_apoapsis = bapo
-        # self.period = (int)(2*math.pi*(self.a**3/(6.673e-11*planet.mass))**.5)
-
         self.periapsis = peri
         self.apoapsis = apo
         if pos is None:
@@ -30,22 +20,26 @@ class Satellite(object):
             self.pos = pos
         self.rx = 0
         self.ry = 0
-
-
         self.planet = planet
-        self.calculator = calc.Orbit_Calculator(self.planet, self.pos, apo, peri,)
+        self.calculator = calc.Orbit_Calculator(self.planet, self.pos, apo, peri, bapo)
 
-        self.del_t = self.calculator.elapsed_time(bapo)+1
-        self.true_anomaly = self.calculator.calc_true_anom(bapo = self.before_apoapsis)
+        self.calculator.area = self.calculator.calculate_area(0, self.calculator.approx_tru_anom(1, True, 0))
+        self.calculator.init_t_anon = self.calculator.approx_tru_anom(1, True, 0)
+        self.t_anomalies = self.calculator.init_angles()
+
+        self.del_t = self.calculator.elapsed_time(bapo)
+        self.true_anomaly = self.calculator.calc_true_anom(bapo=self.before_apoapsis)
 
 
         self.color = 'k'
         self.size = 5
         self.name = None
 
+    # Calculates whether the satellite is before or after the apoapsis
+    # Updates before_apoapsis value
     def calc_bapo(self):
         if self.del_t >= self.calculator.T/2 and self.del_t < self.calculator.T:
-            self.before_apoapsis = False
+            self.before_apoapsis = self.calculator.bapo = False
 
         elif self.del_t >= self.calculator.T:
             time = self.del_t - self.calculator.T
@@ -54,46 +48,33 @@ class Satellite(object):
             self.calc_bapo()
 
         else:
-            self.before_apoapsis = True
+            self.before_apoapsis = self.calculator.bapo = True
 
 
     # Updates elapsed time
     def update_del_t(self, time):
-        self.del_t = self.del_t + time
+        self.del_t = self.calculator.del_t = self.del_t + time
 
-    def update_pos(self, time=5):
-        self.pos = self.calculator.calculate_position(time, self.before_apoapsis)
-        self.calculator.pos = self.pos
+    # Updates satellite position given a change in time
+    def update_pos(self, time=1):
         self.update_del_t(time)
+        self.pos = self.calculator.calculate_Gposition(self.t_anomalies[self.del_t])
+        self.calculator.pos = self.pos
         self.calc_bapo()
-        self.true_anomaly = self.calculator.calc_true_anom(bapo=self.before_apoapsis)
+        self.true_anomaly = self.calculator.true_anomaly = self.t_anomalies[self.del_t]
         self.rx = self.get_x()
         self.ry = self.get_y()
 
+    # Predicts satellite position values given a change in time
+    # Returns list [position, true anomaly, x-component, y-component]
     def predict_pos(self, time=1):
         values = [None]*4
-        self.pos = self.calculator.calculate_position(time, self.before_apoapsis)
-        values[0] = self.pos
-        values[1] = self.calculator.true_anomaly
+        pos = self.calculator.calculate_Gposition(self.t_anomalies[self.del_t+time])
+        values[0] = pos
+        values[1] = self.t_anomalies[self.del_t+time]
         values[2] = values[0] * math.cos(values[1])
         values[3] = values[0] * math.sin(values[1])
-        self.pos = self.calculator.pos
-        self.calculator.true_anomaly = self.true_anomaly
         return values
-
-    def pos_update(self):
-        time = 1
-        nu = self.calculator.approx_tru_anom(time, self.before_apoapsis, self.true_anomaly)
-        self.true_anomaly = self.calculator.true_anomaly = nu
-        self.pos = self.calculator.calculate_Gposition(nu)
-        self.calculator.pos = self.pos
-        self.update_del_t(time)
-        self.calc_bapo()
-        self.rx = self.get_x()
-        self.ry = self.get_y()
-
-
-
 
     def get_time(self):
         return self.del_t
@@ -128,52 +109,5 @@ class Satellite(object):
         return y
 
 
-if __name__ == '__main__':
-    '''
-    mass = float(input("Enter mass of primary body: "))
-    radius = float(input("Enter radius of primary body: "))
-    apo = float(input("Enter distance from Apoapsis to surface (m): "))
-    peri = float(input("Enter distance from Periapsis to surface (m): "))
-    pos = float(input("Enter satellites initial position: "))
-    del_t = float(input("Enter change in time: "))
-    bapo = input("Before apoapsis: (T/F)")
-
-    #  TEST 1 VALUES
-    '''
-    sat_num = 1
-    mass = 2.646e19
-    radius = 60000
-    apo = 117674
-    peri = 6701
-    pos = 6701
-    #del_t = 3193
-
-
-    #a = calc.calc_semimajor_axis(radA, radP)  # Semi-major axis
-   # e = calc.calc_eccentricity(radA, radP)
-
-    planet = Body(radius, mass)
-
-    s1 = Satellite(planet, pos, apo, peri, bapo=True)
-    s1.name = 'Sat 1'
-
-    satellites = [None] * sat_num
-    for i in range(sat_num):
-        satellites[i] = s1  # will change this later to be able to add different satellites
-
-
-    '''s1.update_pos(3193)
-    print(s1.get_position())
-    print(s1.get_time())'''
-
-    '''satellites[0].update_pos(3193)
-    print(satellites[0].get_position())
-    print(satellites[0].get_time())'''  # TEST CASE: GOOD
-
-    #grapher.graph(s1)
-
-
-    grapher.ani_graph(satellites)
-    plt.show()
 
 
