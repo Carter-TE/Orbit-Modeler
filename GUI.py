f61c6b7fa3a8dded4b268cc2a0cad5a5fac1d087
Carter-TE <cte12001@gmail.com>
2020-08-05 15:24:33 -0700

    Created Menu Window
    Created menu window in GUI class for the program on start up

--- GUI.py
+++ GUI.py
@@ -1,48 +1,74 @@
 import Satellite
 import OrbitalGraph as grapher
 import math
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import *
 
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.title = 'Orbit Simulator v.2.3'
+        self.left = 10
+        self.top = 10
+        self.width = 800
+        self.height = 500
+        self.init_UI()
 
+    def init_UI(self):
 
-def main():
-    sat_num = 1
-    mass = 2.646e19
-    radius = 60000
-    apo = 117674
-    peri = 40000
-    pos = 523222
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setFixedSize(self.width, self.height)
+        self.center()
+        self.setStyleSheet("background-image: url(Moon.jpg)")
 
-    planet = Satellite.Body(radius, mass)
-    sats = [None]*2
-    s1 = Satellite.Satellite(planet, apo, peri, pos=peri, bapo=True)
-    print('Sat 1 status: Initialized')
-    s2 = Satellite.Satellite(planet, 23813, 13106, pos=13106, bapo=True)
-    print('Sat 2 status: Initialized')
-    # s1 = Satellite.Satellite(planet, apo, 13106, pos=apo, bapo=True)
-    print('Done')
-    sats[0] = s1
-    sats[1] = s2
+        # Title label
+        header = QLabel(self)
+        header.setText("Orbit Simulator")
+        header_font = QFont('Arial', 24)
+        header_font.setBold(True)
+        header.setFont(header_font)
+        header.setStyleSheet("color: white")
+        header.setAttribute(Qt.WA_TranslucentBackground)
+        header.setGeometry(70,50,400,200)
 
-    # s2.update_pos(7703000)
+        # Buttons
+        plot_orbit = self.create_button("Graph Orbits")
+        ani_orbit = self.create_button('Animate Orbits')
+        plot_torbit = self.create_button('Graph Transfer Orbits')
+        ani_torbit = self.create_button('Animate Transfer Orbits')
 
+        # Button Box
+        button_box = QDialogButtonBox(Qt.Vertical, self)
+        button_box.addButton(plot_orbit, QDialogButtonBox.ActionRole)
+        button_box.addButton(ani_orbit, QDialogButtonBox.ActionRole)
+        button_box.addButton(plot_torbit, QDialogButtonBox.ActionRole)
+        button_box.addButton(ani_torbit, QDialogButtonBox.ActionRole)
+        button_box.setGeometry(50,250,400,300)
 
+        self.show()
 
-    grapher.animate_transfer(sats, s2)
-    # grapher.graph_transfer(sats, s2)
-    # grapher.animate_orbit(sats)
-    # grapher.graph_orbit(sats)
+    # Helper method to center main window
+    def center(self):
+        d = self.frameGeometry()
+        cd = QDesktopWidget().availableGeometry().center()
+        d.moveCenter(cd)
+        print(d.topLeft())
+        self.move(d.topLeft())
 
-
+    # Helper method to create buttons
+    def create_button(self, label):
+        button = QPushButton(label, self)
+        button.setStyleSheet("color: white")
+        button.setFont(QFont('Arial', 14))
+        return button
 
 
 
 if __name__ == '__main__':
-    main()
-    '''
-       mass = float(input("Enter mass of primary body: "))
-       radius = float(input("Enter radius of primary body: "))
-       apo = float(input("Enter distance from Apoapsis to surface (m): "))
-       peri = float(input("Enter distance from Periapsis to surface (m): "))
-       pos = float(input("Enter satellites initial position: "))
-       del_t = float(input("Enter change in time: "))
-       bapo = input("Before apoapsis: (T/F)")'''
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
