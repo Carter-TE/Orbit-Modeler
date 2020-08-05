f93821c98eebf2b6f24169deb79d1422a18c48d4
Carter-TE <cte12001@gmail.com>
2020-08-04 18:31:49 -0700

    Added GUI class

---
+++ GUI.py
@@ -0,0 +1,48 @@
+import Satellite
+import OrbitalGraph as grapher
+import math
+
+
+
+def main():
+    sat_num = 1
+    mass = 2.646e19
+    radius = 60000
+    apo = 117674
+    peri = 40000
+    pos = 523222
+
+    planet = Satellite.Body(radius, mass)
+    sats = [None]*2
+    s1 = Satellite.Satellite(planet, apo, peri, pos=peri, bapo=True)
+    print('Sat 1 status: Initialized')
+    s2 = Satellite.Satellite(planet, 23813, 13106, pos=13106, bapo=True)
+    print('Sat 2 status: Initialized')
+    # s1 = Satellite.Satellite(planet, apo, 13106, pos=apo, bapo=True)
+    print('Done')
+    sats[0] = s1
+    sats[1] = s2
+
+    # s2.update_pos(7703000)
+
+
+
+    grapher.animate_transfer(sats, s2)
+    # grapher.graph_transfer(sats, s2)
+    # grapher.animate_orbit(sats)
+    # grapher.graph_orbit(sats)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+    '''
+       mass = float(input("Enter mass of primary body: "))
+       radius = float(input("Enter radius of primary body: "))
+       apo = float(input("Enter distance from Apoapsis to surface (m): "))
+       peri = float(input("Enter distance from Periapsis to surface (m): "))
+       pos = float(input("Enter satellites initial position: "))
+       del_t = float(input("Enter change in time: "))
+       bapo = input("Before apoapsis: (T/F)")'''
